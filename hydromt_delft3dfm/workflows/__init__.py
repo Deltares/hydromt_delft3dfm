e253bfac748067e124a75c27b5598f7546705704
Xiaohan Li <xiaohan.li@deltares.nl>
2023-08-22 17:51:15 +0200

    small cleanup

--- hydromt_delft3dfm/workflows/__init__.py
+++ hydromt_delft3dfm/workflows/__init__.py
@@ -4,7 +4,6 @@ from .boundaries import *
 from .branches import *
 from .crosssections import *
 from .dem import *
-from ..graph_utils import *
 from .helper import *
 from .manholes import *
 from .mesh import *
