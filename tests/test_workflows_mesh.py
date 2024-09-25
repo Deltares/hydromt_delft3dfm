f05d8cfd658a40608f7250e721655f2f42da233e
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-09-25 21:58:53 +0200

    enabled mesh test

--- tests/test_workflows_mesh.py
+++ tests/test_workflows_mesh.py
@@ -14,16 +14,16 @@ def test_setup_inks1d2d_add_links(tmpdir):
     model = DFlowFMModel(root=join(EXAMPLEDIR, "dflowfm_piave"), mode="r")
     model.read()
     model.set_root(tmpdir, mode="w")
-    # TODO: uncomment all options below
-    # ValueError: These variables cannot be found in this dataset: ['link1d2d_long_names', 'link1d2d_ids']
-    # model.setup_link1d2d(link_direction= "1d_to_2d", link_type="embedded")
-    # AttributeError: type object 'GeometryList' has no attribute 'inner_outer_separator'
-    # model.setup_link1d2d(link_direction= "2d_to_1d", link_type="embedded")
-    # ValueError: These variables cannot be found in this dataset: ['link1d2d_long_names', 'link1d2d_ids']
-    # model.setup_link1d2d(link_direction= "1d_to_2d", link_type="lateral")
-    # AttributeError: 'GeometryList' object has no attribute 'to_geometry'
+    
+    # TODO: make fixture of model?
+    # TODO: uncomment last case
+    # TODO: add checks with assertions
+    
+    model.setup_link1d2d(link_direction= "1d_to_2d", link_type="embedded")
+    model.setup_link1d2d(link_direction= "2d_to_1d", link_type="embedded")
+    model.setup_link1d2d(link_direction= "1d_to_2d", link_type="lateral")
     # model.setup_link1d2d(link_direction= "2d_to_1d", link_type="lateral")
 
-    # add checks with assertions
+    # much of the validation is done upon writing, so maybe write the result also
     model.write_mesh()
     model.write()
