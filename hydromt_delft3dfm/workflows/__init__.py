0125e264be12a62eb00fde16a027c9fc1eba429a
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-10-16 07:52:19 +0200

    fixed linting

--- hydromt_delft3dfm/workflows/__init__.py
+++ hydromt_delft3dfm/workflows/__init__.py
@@ -4,9 +4,9 @@ from .boundaries import *
 from .branches import *
 from .crosssections import *
 from .dem import *
+from .graph import *
 from .manholes import *
 from .mesh import *
-from .graph import *
 from .region import *
 from .roughness import *
 from .structures import *
