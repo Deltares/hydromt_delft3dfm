4ffb33af9f4bb5147fbe93b7710fe462a56f3417
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2026-04-01 17:11:26 +0200

    write geoms only once (#224)
    * write geoms only once
    
    * updated changelog

--- hydromt_delft3dfm/dflowfm.py
+++ hydromt_delft3dfm/dflowfm.py
@@ -3310,10 +3310,6 @@ class DFlowFMModel(MeshModel):
             for name, gdf in self.mesh_gdf.items():
                 self.set_geoms(gdf, name)
 
-        # Write geojson equivalent of all objects.
-        # Note that these files are not directly used when updating the model
-        super().write_geoms(fn="geoms/{name}.geojson")
-
         # Write dfm files
         savedir = dirname(join(self.root, self._config_fn))
 
@@ -3378,6 +3374,8 @@ class DFlowFMModel(MeshModel):
         for name in self.geoms:
             self.set_geoms(convert_lists_to_strings(self.geoms[name]), name)
 
+        # Write geojson equivalent of all objects.
+        # Note that these files are not directly used when updating the model
         super().write_geoms(fn="geoms/{name}.geojson")
 
     def read_forcing(
