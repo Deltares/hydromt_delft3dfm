c4142144cebc042f044947e30835fd275e01f0f6
veenstrajelmer <veenstrajelmer@gmail.com>
2026-04-29 14:04:06 +0200

    minimized diff

--- tests/test_dflowfm.py
+++ tests/test_dflowfm.py
@@ -69,11 +69,6 @@ def test_write_readonlymode(tmpdir, caplog):
         mode="w",
         crs=3857,
     )
-    # TODO: cannot read the crs of a model if no mesh was included
-    mod1.setup_mesh2d(
-        region=dict(bbox=[12.4331, 46.4661, 12.5212, 46.5369]),
-        res=500,
-    )
     mod1.write()
     
     mod2 = DFlowFMModel(root=root, mode="r")
