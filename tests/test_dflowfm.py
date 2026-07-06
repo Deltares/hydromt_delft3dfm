dabd58e9c6c51c0fd4bcae4b0de7216c889c4555
veenstrajelmer <veenstrajelmer@gmail.com>
2026-07-06 17:12:34 +0200

    removed commented code

--- tests/test_dflowfm.py
+++ tests/test_dflowfm.py
@@ -325,9 +325,6 @@ def test_setup_rivers_from_dem(tmpdir):
     # the river width values changed after a bugfix in pyflwdir, more info in
     # https://github.com/Deltares/hydromt_delft3dfm/issues/297
     rivwidth_actual = model.geoms.data["rivers"]["width"].values
-    # rivwidth_expected = np.array(
-    #     [60.50078133, 63.595319  , 50.        , 60.93007025, 60.93007025,
-    #      ]) # pyflwdir pypi
     rivwidth_expected = np.array(
         [55.000397  , 64.28401   , 50.        , 60.93007025, 60.93007025,
          ]) # pyflwdir main
