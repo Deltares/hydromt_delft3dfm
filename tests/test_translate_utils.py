eaed52d607296dd026aa18503a07b3248c00580e
veenstrajelmer <veenstrajelmer@gmail.com>
2026-08-18 14:56:28 +0200

    add test

--- tests/test_translate_utils.py
+++ tests/test_translate_utils.py
@@ -1,5 +1,8 @@
 import pytest
-from hydromt_delft3dfm.utils.translate_utils import varname_to_dflowfm_quantity
+from hydromt_delft3dfm.utils.translate_utils import (
+    meteo_unit_from_type,
+    varname_to_dflowfm_quantity,
+)
 from hydromt_delft3dfm.utils.translate_utils import (
     DICT_VARNAME_TO_DFLOWFM,
     METEO_UNITS,
@@ -28,6 +31,13 @@ def test_varname_to_dflowfm_quantity_notpresent():
     with pytest.raises(KeyError) as e:
         _ = varname_to_dflowfm_quantity(varname)
     assert "varname u10_typo not in keys or values of translation" in str(e.value)
+
+
+def test_meteo_unit_from_type():
+    with pytest.raises(KeyError) as e:
+        _ = meteo_unit_from_type("aaa")
+    assert "Unsupported meteo_type 'aaa'. Supported values are" in str(e.value)
+
 
 def test_consistency_varnames_units():
     exceptions = ["sea_surface_temperature"]
